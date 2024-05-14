04d9abef7f0657c8dec3665ccfa7a137c01ded68
somanigp <somanigp12@gmail.com>
2024-05-14 21:25:12 +0530

    Masking API keys

--- programming/authentication_and_environment_variables_sms/environment_variable.sh
+++ programming/authentication_and_environment_variables_sms/environment_variable.sh
@@ -2,12 +2,12 @@
 # Use these commands in linux terminals. export - to set Env Variables in OS.
 # NOTE: the values of these variables are strings
 # key=value
-export OPEN_WEATHER_API_KEY=783a52292572a6f5701e995dd9e56bdf;  # ; to end a sentence
+#export OPEN_WEATHER_API_KEY={OPEN_WEATHER_API_KEY};  # ; to end a sentence
 export OPEN_WEATHER_URL=https://api.openweathermap.org/data/2.5/forecast;
 export LATITUDE=21.152451;  # My location
 export LONGITUDE=79.080559;
 export TWILIO_ACCOUNT_SID=AC1262acc54d2d3ebe612f77dd7779a03b;
-export TWILIO_AUTH_TOKEN=d315097f0253c75bbcd91ebc436e4c56;
+#export TWILIO_AUTH_TOKEN={TWILIO_AUTH_TOKEN};
 #export LATITUDE=47.218372;
 #export LONGITUDE=-1.553621;
 python main.py
